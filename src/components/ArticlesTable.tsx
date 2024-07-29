ba98e0b434c315349af57ebf88cef1eb220e021a
atilla <atilla1993@hotmail.se>
2024-07-29 11:16:27 +0200

    added table body component

--- src/components/ArticlesTable.tsx
+++ src/components/ArticlesTable.tsx
@@ -1,5 +1,5 @@
 import { Article } from "../services/fakeArticleService";
-import { getAcronym } from "../utils";
+import TableBody from "./TableBody";
 import TableHeader, { Column } from "./TableHeader";
 
 export interface SortColumn {
@@ -30,31 +30,7 @@ function ArticlesTable({ articles, sortColumn, onSort, onDelete }: Props) {
   return (
     <table className="table">
       <TableHeader onSort={onSort} sortColumn={sortColumn} columns={columns} />
-      <tbody>
-        {articles.map((article) => (
-          <tr>
-            <td>{article.category.name}</td>
-            <td>
-              {article.title} ({getAcronym(article.title)})
-            </td>
-            <td>{article.author}</td>
-            <td>{article.nbrPages}</td>
-            <td>{article.type}</td>
-            <td>{article.runTimeMinutes}</td>
-            <td>{article.isBorrowable ? "Available" : "N/A"}</td>
-            <td>{article.borrower}</td>
-            <td>{article.borrowDate}</td>
-            <td>
-              <button
-                className="btn btn-danger"
-                onClick={() => onDelete(article._id)}
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
+      <TableBody articles={articles} onDelete={onDelete} />
     </table>
   );
 }
